7a0f52244aea79846fd11eed0608e044f33bbce5
Dima Belitskiy <dbelitsky@coinmotion.com>
2023-06-27 22:08:07 +0300

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Simple React.js app
+
+Build used vite.js, react, tailwindcss
+
+## Local setup
+
+Clone, install npm modules, run.
+
+Now little more details:
+
+```
+git clone ...
+cd app
+npm install
+npm run dev
+```
+
+Open localhost.
+
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <form></form>
+        <div className="sorting-filter"></div>
+        <div className="units-selector"></div>
+      </header>
+
+      <section className="content"></section>
+    </>
+  )
+}
+
+export default App
